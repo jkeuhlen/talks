52d474f46cd5653c36d97c3ec93697e7467e433d
Jake Keuhlen <jakeuhlen@hollandhart.com>
2018-05-20 21:33:39 -0600

    Preliminary version of eval

--- extensibly-free-arrows/src/Arrows.hs
+++ extensibly-free-arrows/src/Arrows.hs
@@ -18,7 +18,7 @@ data FreeA eff a b where
     Pure :: (a -> b) -> FreeA eff a b
     Effect :: eff a b -> FreeA eff a b
     Seq :: FreeA eff a b -> FreeA eff b c -> FreeA eff a c
-    Par :: FreeA eff a₁ b₁ -> FreeA eff a₂ b₂ -> FreeA eff (a₁, a₂) (b₁, b₂)
+    Par :: FreeA eff a1 b1 -> FreeA eff a2 b2 -> FreeA eff (a1, a2) (b1, b2)
     -- Apply -- | Arrow apply 
     -- FanIn -- | Arrow Choice
     -- Spl   -- | Arrow Choice
@@ -32,19 +32,34 @@ instance C.Category (FreeA eff) where
 
 instance Arrow (FreeA eff) where
     arr = Pure
-    first f = Par f id
-    second f = Par id f
+    first f = Par f C.id
+    second f = Par C.id f
     (***) = Par
 
-evalA :: forall eff arr a₀ b₀. (Arrow arr) => (forall a b. eff a b -> arr a b) -> FreeA eff a₀ b₀ -> arr a₀ b₀
+evalA :: forall eff arr a0 b0. (Arrow arr) => (forall a b. eff a b -> arr a b) -> FreeA eff a0 b0 -> arr a0 b0
 evalA exec = go
   where
-    go :: forall a b. FreeA eff a b -> arr a b
+    go :: forall a b . (Arrow arr) => FreeA eff a b -> arr a b
     go freeA = case freeA of
         Pure f -> arr f
-        Seq f₁ f₂ -> go f₂ . go f₁
-        Par f₁ f₂ -> go f₁ *** go f₂
+        Seq f1 f2 -> go f2 C.. go f1
+        Par f1 f2 -> go f1 *** go f2
         Effect eff -> exec eff
+
+-- evalA :: forall m a b . 
+--   (
+--     Monad m
+--   ) => FreeA (Kleisli m) a b 
+--     -> Kleisli m a b
+-- evalA = go
+--   where
+--     go :: (Monad m) => FreeA (Kleisli m) a b -> Kleisli m a b
+--     go freeA = case freeA of
+--         Pure f -> Kleisli $ return . f
+--         Effect eff -> eff
+--         Seq f1 f2 -> go f2 C.. go f1
+--         Par f1 f2 -> go f1 *** go f2
+
 
 liftK :: Monad m => (b -> m c) -> FreeA (Kleisli m) b c
 liftK eff = effect (Kleisli $ \x -> eff x)

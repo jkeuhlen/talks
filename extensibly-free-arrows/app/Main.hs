842583dd90259f6ef7570efd71262435a32d5e7b
Jake Keuhlen <jakeuhlen@hollandhart.com>
2018-05-24 07:48:25 -0600

    Setting up extensible

--- extensibly-free-arrows/app/Main.hs
+++ extensibly-free-arrows/app/Main.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Arrows
-
+import Control.Arrow 
+import Data.Text (Text)
 main :: IO ()
-main = putStrLn "Hello world"
+main = do 
+  i <- getLine 
+  case i of 
+    "1" -> runKleisli (evalKleisliA $ evalA cmplPrintX $ effect Screen >>> Pure (const "foo") >>> effect Screen) ("'ello World!" :: Text)
+    _   -> runKleisli (evalKleisliA $ evalA cmplPrintXToFile $ effect Screen) ("'ello File!" :: Text)
+
+
+
+extensibleArrow :: (eff :>+: PrintX, eff :>+: Print2X) => FreeA eff Text b
+extensibleArrow = proc x -> do 
+   
--- extensibly-free-arrows/src/Arrows.hs
+++ extensibly-free-arrows/src/Arrows.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE Arrows #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 module Arrows where
 
 import Data.Extensible.Sum
@@ -9,10 +10,13 @@ import Data.Extensible.Sum2
 import Data.Extensible.Product
 import Data.Text(Text)
 import Control.Arrow
+import qualified Data.Text.IO as T
 import qualified Control.Category as C
 import Control.Monad
 import Control.Monad.IO.Class
 
+
+import Debug.Trace
 -- Free Arrow
 data FreeA eff a b where
     Pure :: (a -> b) -> FreeA eff a b
@@ -46,28 +50,67 @@ evalA exec = go
         Par f1 f2 -> go f1 *** go f2
         Effect eff -> exec eff
 
--- evalA :: forall m a b . 
---   (
---     Monad m
---   ) => FreeA (Kleisli m) a b 
---     -> Kleisli m a b
--- evalA = go
---   where
---     go :: (Monad m) => FreeA (Kleisli m) a b -> Kleisli m a b
---     go freeA = case freeA of
---         Pure f -> Kleisli $ return . f
---         Effect eff -> eff
---         Seq f1 f2 -> go f2 C.. go f1
---         Par f1 f2 -> go f1 *** go f2
+evalKleisliA :: forall m a b . 
+  (
+    Monad m
+  ) => FreeA (Kleisli m) a b 
+    -> Kleisli m a b
+evalKleisliA = go
+  where
+    go :: forall m a b . (Monad m) => FreeA (Kleisli m) a b -> Kleisli m a b
+    go freeA = case freeA of
+        Pure f -> Kleisli $ return . f
+        Effect eff -> eff
+        Seq f1 f2 -> go f2 C.. go f1
+        Par f1 f2 -> go f1 *** go f2
 
 
 liftK :: Monad m => (b -> m c) -> FreeA (Kleisli m) b c
 liftK eff = effect (Kleisli $ \x -> eff x)
 
--- Free effect
+-- Free effects
 data PrintX a b where 
   Screen :: PrintX Text () 
 
 cmplPrintX :: (MonadIO m) => PrintX a b -> FreeA (Kleisli m) a b 
-cmplPrintX Screen = liftK $ const $ liftIO $ putStrLn "Foo!"
+cmplPrintX Screen = liftK (\x -> liftIO $ T.putStrLn x)
 
+cmplPrintXToFile :: (MonadIO m) => PrintX a b -> FreeA (Kleisli m) a b 
+cmplPrintXToFile Screen = liftK (\x -> liftIO $ T.writeFile "output.txt" x)
+
+data Print2X a b where 
+  Screen2 :: Print2X String ()
+
+cmplPrint2X :: (MonadIO m) => Print2X a b -> FreeA (Kleisli m) a b 
+cmplPrint2X Screen2 = liftK (\x -> liftIO $ putStrLn x)
+
+cmplPrint2XToFile :: (MonadIO m) => Print2X a b -> FreeA (Kleisli m) a b 
+cmplPrint2XToFile Screen2 = liftK (\x -> liftIO $ writeFile "output.txt" x)  
+
+
+-- Sum Class information 
+liftL :: FreeA f a b -> FreeA (f :+: g) a b
+liftL = fmapEff InL
+
+liftR :: FreeA g a b -> FreeA (f :+: g) a b
+liftR = fmapEff InR
+
+lftEff :: (Sum2 eff f) 
+  => FreeA f a b 
+  -> FreeA eff a b
+lftEff = fmapEff lft2
+
+lftE :: (eff :>+: f)
+  => f a b -> FreeA eff a b
+lftE = lftEff . effect
+
+fmapEff :: forall a b c eff1 eff2 . (forall bb cc . eff1 bb cc -> eff2 bb cc)
+  -> FreeA eff1 b c -> FreeA eff2 b c
+fmapEff fxn = go
+  where
+    go :: FreeA eff1 a b -> FreeA eff2 a b 
+    go (Effect eff) = effect $ fxn eff 
+    go x = x 
+    -- alg :: SArrow a eff1 (FreeA eff2) b' c' -> FreeA eff2 b' c'
+    -- alg (Effect eff) = effect $ fxn eff
+    -- alg x = recombineWTagA x
